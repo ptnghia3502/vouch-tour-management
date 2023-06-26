45cb01b40728f2d563c558290af9abe0290ce6eb
Vuong Huu Hieu <ownhieu0804@gmail.com>
2023-06-26 10:19:36 +0700

    category

--- lib/controllers/category_controller.dart
+++ lib/controllers/category_controller.dart
@@ -1,15 +1,29 @@
 import 'dart:convert';
+import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:admin/models/global.dart';
 import 'package:admin/models/category_model.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:path_provider/path_provider.dart';
 class CategoryController extends GetxController{
   static CategoryController instance = Get.find();
+  var currentPage = 0.obs;
+  final itemsPerPage = 10;
   var categoryList = <Category>[].obs;
   var foundCategoryList = <Category>[].obs;
   static String jwtToken = '';
   static String currentEmail = 'hieuvh0804@gmail.com';
+  //image
+  bool imageAvailable = false;
+  late Uint8List imageFile;
+
+
+  //textEditingController
+  TextEditingController categoryNameTextController = TextEditingController(); 
+
 
   @override
   Future<void> onInit() async {
@@ -55,6 +69,8 @@ class CategoryController extends GetxController{
     }
   }
 
+
+//==============get all category=============
   void fetchCategory() async {
     String jwtToken = CategoryController.jwtToken;
 
@@ -89,5 +105,65 @@ class CategoryController extends GetxController{
       results = categoryList.where((element) => element.categoryName.toString().toLowerCase().contains(categoryName.toLowerCase())).toList();
     }
     foundCategoryList.value = results as List<Category>;
+  }
+
+
+    //=====================this method is paging======================
+  List<Category> get currentItems {
+    final startIndex = currentPage.value * itemsPerPage;
+    final endIndex = (startIndex + itemsPerPage).clamp(0, categoryList.length);
+    return foundCategoryList.sublist(startIndex, endIndex);
+  }
+
+  void nextPage() {
+    if (currentPage.value <
+        (foundCategoryList.length / itemsPerPage).ceil() - 1) {
+      currentPage.value++;
+    }
+  }
+
+  void previousPage() {
+    if (currentPage.value > 0) {
+      currentPage.value--;
+    }
+  }
+
+  //==================insert Catefory===========
+  Future<File> uint8ListToFile(Uint8List imageInUnit8List) async {
+    final tempDir = await getTemporaryDirectory();
+    File file = await File('${tempDir.path}/image.png').create();
+  file.writeAsBytesSync(imageInUnit8List);
+    return file;
+    
+  }
+
+  Future<bool> insertCategory() async {
+    try{
+      String jwtToken = CategoryController.jwtToken;
+
+      if (jwtToken.isEmpty) {
+        jwtToken = await CategoryController.fetchJwtToken(CategoryController
+            .currentEmail); // Fetch the JWT token if it's empty
+      }
+      final Map<String, String> headers = {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer $jwtToken'
+      };
+      var url = Uri.parse('${BASE_URL}categories');
+      Map body = {
+        'categoryName' : categoryNameTextController.text,
+        'File' : uint8ListToFile(imageFile)
+      };
+      http.Response response = await http.post(url, body: jsonEncode(body), headers: headers);
+      if(response.statusCode == 201){
+        print('Post success');
+        return true;
+      }
+
+    }
+    catch(e){
+
+    }
+    return false;
   }
 }
--- lib/screens/category/components/category_header_table.dart
+++ lib/screens/category/components/category_header_table.dart
@@ -1,7 +1,9 @@
-
 import 'package:admin/responsive.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker_web/image_picker_web.dart';
 import '../../../constants.dart';
+
 class CategoryHeaderTable extends StatelessWidget {
   const CategoryHeaderTable({
     Key? key,
@@ -16,10 +18,10 @@ class CategoryHeaderTable extends StatelessWidget {
           children: [
             Text(
               "Danh sách các loại sản phẩm",
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                color: Colors.lightBlue,
-                fontSize: 22
-              ),
+              style: Theme.of(context)
+                  .textTheme
+                  .titleMedium
+                  ?.copyWith(color: Colors.lightBlue, fontSize: 22),
             ),
             ElevatedButton.icon(
               style: TextButton.styleFrom(
@@ -29,9 +31,92 @@ class CategoryHeaderTable extends StatelessWidget {
                       defaultPadding / (Responsive.isMobile(context) ? 2 : 1),
                 ),
               ),
-              onPressed: () {},
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        backgroundColor: bgColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        scrollable: true,
+                        title: Center(
+                          child: Text('TẠO MỚI NHÀ CUNG CẤP',
+                              style:
+                                  TextStyle(fontSize: 16, color: Colors.white)),
+                        ),
+                        content: Container(
+                          width: 700,
+                          child: Padding(
+                            padding: const EdgeInsets.all(20.0),
+                            child: Form(
+                              child: Container(
+                                width: 500,
+                                color: secondaryColor,
+                                child: Column(
+                                  children: <Widget>[
+                                    TextFieldForFroms(
+                                        label: 'Tên Thể Loại',
+                                        validationResult:
+                                            'Tên thể loại không được bỏ trống',
+                                        textEditingController:
+                                            categoryController
+                                                .categoryNameTextController,
+                                        icondata: Icons.message),
+                                    UploadImage(),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                        actions: [
+                          ElevatedButton(
+                            onPressed: () => Navigator.pop(context),
+                            child: Text(
+                              'Hủy',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                          ElevatedButton(
+                            onPressed: () async {
+                              // Handle delete button press
+                              bool isInserted =
+                                  await categoryController.insertCategory();
+
+                              if (isInserted) {
+                                Fluttertoast.showToast(
+                                  msg: 'Thêm mới thành công',
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 10,
+                                  backgroundColor: Colors.green,
+                                  textColor: Colors.white,
+                                );
+                              } else {
+                                Fluttertoast.showToast(
+                                  msg: 'Có lỗi rồi!',
+                                  toastLength: Toast.LENGTH_SHORT,
+                                  gravity: ToastGravity.BOTTOM,
+                                  timeInSecForIosWeb: 10,
+                                  backgroundColor: Colors.red,
+                                  textColor: Colors.white,
+                                );
+                              }
+                              Navigator.pop(context);
+                            },
+                            child: Text(
+                              'Gửi',
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ),
+                        ],
+                      );
+                    });
+              },
               icon: Icon(Icons.add),
-              label: Text("Add New"),
+              label: Text("Tạo mới thể loại"),
             ),
           ],
         ),
@@ -40,4 +125,91 @@ class CategoryHeaderTable extends StatelessWidget {
   }
 }
 
+// ignore: must_be_immutable
+class TextFieldForFroms extends StatelessWidget {
+  String label;
+  String validationResult;
+  IconData icondata;
+  TextEditingController textEditingController;
+  TextFieldForFroms({
+    super.key,
+    required this.label,
+    required this.validationResult,
+    required this.textEditingController,
+    required this.icondata,
+  });
 
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Container(
+        width: 400,
+        child: TextFormField(
+          controller: this.textEditingController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            labelText: this.label,
+            icon: Icon(this.icondata),
+          ),
+          validator: (value) {
+            if (value!.isEmpty) {
+              return this.validationResult;
+            }
+            return null;
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class UploadImage extends StatefulWidget {
+  const UploadImage({super.key});
+
+  @override
+  State<UploadImage> createState() => _UploadImageState();
+}
+
+class _UploadImageState extends State<UploadImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Container(
+              width: 400,
+              child: Container(
+                height: 120,
+                width: 120,
+                color: Colors.blue,
+                child: categoryController.imageAvailable
+                    ? Image.memory(categoryController.imageFile)
+                    : const SizedBox(),
+              )),
+        ),
+        Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Container(
+            width: 400,
+            child: GestureDetector(
+                onTap: () async {
+                  final image = await ImagePickerWeb.getImageAsBytes();
+                  setState(() {
+                    categoryController.imageFile = image!;
+                    categoryController.imageAvailable = true;
+                  });
+                },
+                child: Container(
+                  height: 50,
+                  width: 150,
+                  color: Colors.blue,
+                  child: Center(child: Text('Chọn hình')),
+                )),
+          ),
+        )
+      ],
+    );
+  }
+}
